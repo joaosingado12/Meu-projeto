078d603fc01b306e1a42bf19efc14a61b07d5e81
João Costa <joao21pedro21.costa@gmail.com>
2023-07-11 16:47:16 -0300

    Initial commit

---
+++ contas/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" %now
+    return HttpResponse(html)
+
+def teste(request):
+    return render(request, 'index.html')
+# Create your views here.
